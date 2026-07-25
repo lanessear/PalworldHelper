c03f9cba3db09332602f8a01889db76558afb257
lanessear <shirin_@live.de>
2026-07-25 21:08:16 +0200

    fix: use invariant timestamp for update archive

--- src/PalworldHelper/ApplicationMaintenanceService.cs
+++ src/PalworldHelper/ApplicationMaintenanceService.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.IO.Compression;
 using System.Net.Http;
@@ -77,7 +78,7 @@ public static class ApplicationMaintenanceService
         var executable = Environment.ProcessPath ?? Path.Combine(installDirectory, "PalworldHelper.exe");
         var archiveDirectory = Path.Combine(
             Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-            "PalworldHelper", "obsolete", DateTime.Now.ToString("yyyyMMdd-HHmmss"));
+            "PalworldHelper", "obsolete", DateTime.Now.ToString("yyyyMMdd-HHmmss", CultureInfo.InvariantCulture));
         var scriptPath = Path.Combine(root, "apply-update.ps1");
         var script = BuildUpdateScript(Environment.ProcessId, installDirectory, staging, archiveDirectory, executable, root);
         await File.WriteAllTextAsync(scriptPath, script, new UTF8Encoding(false), cancellationToken).ConfigureAwait(false);
